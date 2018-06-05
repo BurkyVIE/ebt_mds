ffde504b34fb3dedcab7b9733b19e077c45c9dbd
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2018-06-05 10:59:37 +0200

    Update and rename ebt_mds_grpd.r to grpd_ebt_mds.r

--- grpd_ebt_mds.r
+++ grpd_ebt_mds.r
@@ -11,14 +11,14 @@ ebt_mds_grpd <- function(period = FALSE, mds_data = ebt_mds) {
   if(!period %in% period_list) period <- period_list[menu(period_list, title = "choose periode")]
   if(identical(period, character(0))) return(NULL)
   
-  # Erstelle volleständige Liste der möglichen Daten und ergänze Periode
+  # Erstelle vollestÃ¤ndige Liste der mÃ¶glichen Daten und ergÃ¤nze Periode
   mds_data$Date %>%
     range() %>%
     full_seq(1) %>%
     tibble(Date = .) %>%
     transmute(Date = Date,
               Period = floor_date(Date, unit = period, week_start = 1)) %>%
-    # Zusammenführen mit den mds-Daten und Zusammenfassen
+    # ZusammenfÃ¼hren mit den mds-Daten und Zusammenfassen
     left_join(ebt_mds, "Date") %>%
     group_by(Period) %>% 
     summarise(Count = sum(Count, na.rm = TRUE),
@@ -30,12 +30,3 @@ ebt_mds_grpd <- function(period = FALSE, mds_data = ebt_mds) {
     select(Period, Count, Value, Avg, Hits, HitRt, nLoc) %>% 
     return()
   }
-
-
-  
-ebt_mds_grpd(period = "month") %>% 
-  ggplot(data = ., mapping = aes(x = Period, y = HitRt)) +
-  geom_point(size = 3/2, shape = 4, stroke = 2, color = "mediumblue", alpha = 3/4) +
-  geom_smooth(color = "orangered") +
-#  scale_y_log10() +
-  theme_ebt()
