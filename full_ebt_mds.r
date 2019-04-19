ffaa0028eb2069e0e850792b6eee7449b34af7a3
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2019-04-19 06:49:45 +0200

    Update full_ebt_mds.r

--- full_ebt_mds.r
+++ full_ebt_mds.r
@@ -1,19 +1,17 @@
-library(tidyverse)
-library(magrittr)
-library(lubridate)
-
-# Einlesen des minimal Dataset
-source("ebt_mds.txt")
+source("ebt_mds.txt") # Einlesen des minimal Dataset
 
 ebt_mds_full <-
-  ebt_mds$Date %>%
-  full_seq(1) %>%
-  tibble(Date = .) %>%
-  left_join(ebt_mds, by = "Date") %>%
-  mutate(Avg = Value / Count,
-         HitRt = Count / Hits,
-         nLoc = map(Loc, length) %>% unlist(),
-         Day = c("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")[wday(Date, week_start = 1)] %>%
-           parse_factor(levels = c("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"), ordered = TRUE)) %>% 
-  replace_na(replace = list("Count" = 0L, Value = 0L, Hits = 0L)) %>% 
-  select(Date, Day, Count, Value, Avg, Hits, HitRt, nLoc, Loc)
+  ebt_mds$Date %>%                    # aus den Eingabezeitpunkten ...
+  full_seq(1) %>%                     # einen Vektor aller möglichen Zeitpunkte erzeugen ...
+  tibble(Date = .) %>%                # in einen Tibble umwandeln ...
+  left_join(ebt_mds, by = "Date") %>% # und die Daten zu den Eingabezeitpunkten einfügen.
+  mutate(Day = lubridate::wday(x = Date, week_start = 1, label = TRUE),
+         Deno = purrr::map(.x = Deno, .f = ~ if(is.null(.)) integer(7) else as.integer(.)), # ersetze die aus dem Joinen (auch schon bei der Erstellung) entstandenen NULL
+         Count = map_int(.x = Deno, .f = ~ sum(.)),
+         Value = map_int(.x = Deno, .f = ~ (t(.) %*% c(5, 10, 20, 50, 100, 200, 500)) %>% as.integer()),
+         Hits = as.integer(Hits),
+         nLoc = map_int(.x = Loc, .f = ~ length(.))) %>%
+  #replace_na(replace = list(Count = 0L, Value = 0L, Hits = 0L)) %>% 
+  select(Date, Day, Deno, Count, Value, Hits, Loc, nLoc)
+# Immer englische Abkürzungen für die Wochentage verwenden [wday() nimmt aus locale()]
+levels(ebt_mds_full$Day) <- c("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")
