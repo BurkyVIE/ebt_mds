5d04c0e2e033d3a454b7d839ecff750987f9436b
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2019-08-02 09:08:59 +0200

    Update ebt_mds_full.r

--- ebt_mds_full.r
+++ ebt_mds_full.r
@@ -4,3 +4,32 @@ source("ebt_mds.txt") # Einlesen des minimal Dataset
 source("ebt_mds_grpd.r") # Funktion zum Gruppieren
 
 ebt_mds_full <- ebt_mds_grpd(per = "day")
+
+# Kennzahlen
+library(tidyverse)
+bind_cols(
+  ebt_mds_full %>% summarise_at(.vars = vars(Days, Count, Value, Hits), .funs = ~sum(., na.rm = TRUE)),
+  ebt_mds_full %>% summarise(nLoc = map_int(.x = list(Reduce(union, Loc)), .f = ~ length(.)))
+) %>%
+  print()
+
+
+
+### Expandieren der Denos
+#ebt_mds_full %>% 
+#  bind_cols(
+#    ebt_mds_full$Deno %>%
+#      do.call(rbind, .) %>%
+#      as_tibble(.name_repair = "minimal") %>%
+#      setNames(., paste0("E", c(5, 10, 20, 50, 100, 200, 500) %>% sprintf("%03d", .)))
+#  )
+
+
+
+### Wo war ich am
+# pseudo %>%
+#   filter(Loc %in% (ebt_mds_full %>%
+#            filter(Date %in% lubridate::make_date(year = 2004:2019, month = 3, day = 21)) %>%
+#            pull(Loc) %>% unlist() %>% unique())) %>%
+#   select(Coords, Country:City) %>%
+#   arrange(Country, ZIP)
