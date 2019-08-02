8312d1dd9bc475d297f450918b521816a067858f
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2019-08-02 09:24:41 +0200

    Update ebt_mds_full.r

--- ebt_mds_full.r
+++ ebt_mds_full.r
@@ -3,7 +3,9 @@ library(tidyverse)
 source("ebt_mds.txt") # Einlesen des minimal Dataset
 source("ebt_mds_grpd.r") # Funktion zum Gruppieren
 
-ebt_mds_full <- ebt_mds_grpd(per = "day")
+ebt_mds_full <-
+  ebt_mds_grpd(per = "day") %>% 
+  rename(Date = Period)
 
 # Kennzahlen
 library(tidyverse)
