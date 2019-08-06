46d2a153a0c15146ee646e0bb855c663a0674467
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2019-08-06 09:31:46 +0200

    Update ebt_mds_grpd.r

--- ebt_mds_grpd.r
+++ ebt_mds_grpd.r
@@ -13,13 +13,15 @@ ebt_mds_grpd <- function(period = FALSE, mds_data = ebt_mds) {
   mds_data$Date %>%                          # aus den Eingabezeitpunkten ...
     full_seq(1) %>%                          # einen Vektor aller möglichen Zeitpunkte erzeugen ...
     enframe(name = NULL, value = "Date") %>% # in einen Tibble umwandeln ...
-    left_join(mds_data, by = "Date") %>%          # und die Daten zu den Eingabezeitpunkten einfügen.
-    # Fülle Deno auf 7 Stellen auf
-    mutate(Deno = map(.x = Deno, .f = function(x = .) c(as.integer(x), integer(7 - length(x))))) %>% 
+    left_join(mds_data, by = "Date") %>%     # und die Daten zu den Eingabezeitpunkten einfügen.
+    # Fülle Deno auf 7 Stellen auf und wandle Hits in integer um
+    mutate(Deno = map(.x = Deno, .f = function(x = .) c(as.integer(x), integer(7 - length(x)))),
+           Hits = as.integer(Hits)) %>% 
     # Ergänze Periode
 #    mutate(Period = ceiling_date(Date, unit = period, week_start = 1) - days(1)) %>% # Führt leider zu ungewohnten Effekten in GRafiken
     mutate(Period = floor_date(Date, unit = period, week_start = 1)) %>%
     group_by(Period) %>% 
+    # Zusammenfassen entsprechend Periode
     summarise(Deno = list(Reduce(`+`, Deno)),
               Loc = list(Reduce(union, Loc)),
               Days = n(),
