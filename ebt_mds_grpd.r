14765cc7c8471e178cd32572c0e6ce4f0736cb20
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2019-08-06 09:05:21 +0200

    Update ebt_mds_grpd.r

--- ebt_mds_grpd.r
+++ ebt_mds_grpd.r
@@ -13,24 +13,24 @@ ebt_mds_grpd <- function(period = FALSE, mds_data = ebt_mds) {
   mds_data$Date %>%                          # aus den Eingabezeitpunkten ...
     full_seq(1) %>%                          # einen Vektor aller möglichen Zeitpunkte erzeugen ...
     enframe(name = NULL, value = "Date") %>% # in einen Tibble umwandeln ...
-    left_join(mds_data, "Date") %>%          # und die Daten zu den Eingabezeitpunkten einfügen.
+    left_join(mds_data, by = "Date") %>%          # und die Daten zu den Eingabezeitpunkten einfügen.
     # Fülle Deno auf 7 Stellen auf
     mutate(Deno = map(.x = Deno, .f = function(x = .) c(as.integer(x), integer(7 - length(x))))) %>% 
     # Ergänze Periode
 #    mutate(Period = ceiling_date(Date, unit = period, week_start = 1) - days(1)) %>% # Führt leider zu ungewohnten Effekten in GRafiken
     mutate(Period = floor_date(Date, unit = period, week_start = 1)) %>%
     group_by(Period) %>% 
-    summarise(Days = n(),
-              Deno = list(Reduce(`+`, Deno)),
+    summarise(Deno = list(Reduce(`+`, Deno)),
               Loc = list(Reduce(union, Loc)),
-              Count = map_int(.x = Deno, .f = ~ sum(.)),
-              Value = map_int(.x = Deno, .f = ~ (t(.) %*% c(5, 10, 20, 50, 100, 200, 500)) %>% as.integer()),
-              Hits = sum(Hits, na.rm = TRUE),
-              nLoc = map_int(.x = Loc, .f = ~ length(.))) %>%
-    # Weitere Ableitungen
-    mutate(EntRt = Count / Days,
+              Days = n(),
+              Hits = sum(Hits, na.rm = TRUE)) %>%
+    # Diverse Ableitungen
+    mutate(Count = map_int(.x = Deno, .f = ~ sum(.)),
+           Value = map_int(.x = Deno, .f = ~ (t(.) %*% c(5, 10, 20, 50, 100, 200, 500)) %>% as.integer()),
+           nLoc = map_int(.x = Loc, .f = ~ length(.)),
            Avg = Value / Count,
            HitRt = Count / Hits,
+           EntRt = Count / Days,
            LocRt = nLoc / Days) %>% 
     return()
   }
