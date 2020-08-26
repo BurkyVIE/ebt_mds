76a8c5e720038530c678373e4b05fd60e3fa0179
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2020-08-26 08:27:35 +0200

    Update yearly entrty rates.r
    added weekly (7-day period) graph

--- yearly entrty rates.r
+++ yearly entrty rates.r
@@ -1,3 +1,5 @@
+# Täglich
+
 library(tidyverse)
 library(lubridate)
 
@@ -13,15 +15,45 @@ ebt_mds_full %>%
   mutate(cumCount = cumsum(Count)) %>% 
   ungroup() %>% 
   ggplot(data = ., mapping = aes(x = Course, y = cumCount)) +
-  geom_abline(slope = c(100, 200), color = "white", linetype = "dashed", size = 1) +
-  geom_line(mapping = aes(group = Year, color = Year2, size = Year2), alpha = 1/2) +
+  geom_abline(slope = c(100, 200, 300), color = "white", linetype = "dashed", size = 1) +
+  geom_line(mapping = aes(group = Year, color = Year2, size = Year2), alpha = .6) +
   scale_color_manual(name = "", values = c(rev(RColorBrewer::brewer.pal(top, "Set1")), "grey")) +
   scale_size_manual(name = "", values = c(rep(2.5, top), 1.5)) +
   scale_x_continuous(name = "Course (day of year)") +
   scale_y_continuous(name = "Cumulative Count [k]", labels = function(x) x / 1000) +
   labs(title = "Entries Throughout the Year") +
-  theme_ebt() -> p
-                     
+  theme_ebt() -> p 
+
+windows(16, 9)
+plot(p)
+
+rm(top, p)
+
+# Wöchentlich
+
+library(tidyverse)
+library(lubridate)
+
+top <- 3
+
+ebt_mds_grpd(per = "week") %>% 
+  mutate(Year = year(Period),
+         Year2 = case_when(Year < (max(Year) - top + 1) ~ "older",
+                           TRUE ~ as.character(Year)),
+         Course = week(Period)) %>% 
+  group_by(Year) %>% 
+  mutate(cumCount = cumsum(Count)) %>% 
+  ungroup() %>% 
+  ggplot(data = ., mapping = aes(x = Course, y = cumCount)) +
+  geom_abline(slope = c(100, 200, 300), color = "white", linetype = "dashed", size = 1) +
+  geom_line(mapping = aes(group = Year, color = Year2, size = Year2), alpha = .6) +
+  scale_color_manual(name = "", values = c(rev(RColorBrewer::brewer.pal(top, "Set1")), "grey")) +
+  scale_size_manual(name = "", values = c(rep(2.5, top), 1.5)) +
+  scale_x_continuous(name = "Course (week = 7 day periods)") +
+  scale_y_continuous(name = "Cumulative Count [k]", labels = function(x) x / 1000) +
+  labs(title = "Entries Throughout the Year") +
+  theme_ebt() -> p 
+
 windows(16, 9)
 plot(p)
 
