8868d571908a5c32f3c485a14ab0c6336e1ce6c0
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2020-08-26 08:29:46 +0200

    Update yearly entrty rates.r

--- yearly entrty rates.r
+++ yearly entrty rates.r
@@ -45,7 +45,7 @@ ebt_mds_grpd(per = "week") %>%
   mutate(cumCount = cumsum(Count)) %>% 
   ungroup() %>% 
   ggplot(data = ., mapping = aes(x = Course, y = cumCount)) +
-  geom_abline(slope = c(100, 200, 300), color = "white", linetype = "dashed", size = 1) +
+  geom_abline(slope = c(100, 200, 300) * 7, color = "white", linetype = "dashed", size = 1) +
   geom_line(mapping = aes(group = Year, color = Year2, size = Year2), alpha = .6) +
   scale_color_manual(name = "", values = c(rev(RColorBrewer::brewer.pal(top, "Set1")), "grey")) +
   scale_size_manual(name = "", values = c(rep(2.5, top), 1.5)) +
