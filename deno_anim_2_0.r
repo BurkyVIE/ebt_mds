e629a4b8874763b4740880ea1900399f7058705c
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2021-02-17 09:38:43 +0100

    Update deno_anim_2_0.r

--- deno_anim_2_0.r
+++ deno_anim_2_0.r
@@ -4,29 +4,29 @@ library(gganimate)
 
 source("ebt_mds_grpd.r")
 
-dat <- ebt_mds_grpd(period = "quarter")
+dat <- ebt_mds_grpd(period = "halfyear")
 dat %>% select(Period, Deno) %>%
-  transmute(Quarter = Period,
+  transmute(Semester = Period,
             map_dfr(Deno, ~set_names(., paste0("EUR_", sprintf("%03d", c(5, 10, 20, 50, 100, 200, 500))))),
-            Q4 = case_when(quarter(Period) == 4 ~ TRUE,
+            H2 = case_when(semester(Period) == 2 ~ TRUE,
                            TRUE ~ NA)) %>%
   mutate(across(starts_with("EUR_"), cumsum)) %>% 
   pivot_longer(starts_with("EUR_"), names_to = "Denomination", values_to = "Count") %>%
   ggplot(mapping = aes(x = Denomination, y = Count, group = Denomination)) +
   geom_col(mapping = aes(fill = Denomination), show.legend = FALSE) +
-  geom_point(mapping = aes(shape = Q4), color = "grey25", na.rm = TRUE, show.legend = FALSE) +
+  geom_point(mapping = aes(shape = H2), color = "grey25", na.rm = TRUE, show.legend = FALSE) +
   scale_x_discrete(labels = paste0(c(5, 10, 20, 50, 100, 200, 500), " EUR")) +
   scale_y_continuous(name = "Count [k]", labels = function(x) x / 1000) +
   scale_fill_manual(values = c("#CCCCCC","#FF9999","#99CCFF","#FFCC99","#66CC66","#FFCC33","#B299CC")) +
   scale_shape_manual(values = c(1)) +
   labs(title = "Denominations entered by Burky",
-       subtitle = "as Quarter {quarter(frame_time, with_year = TRUE)} (cumulated)") +
+       subtitle = "as Semester {semester(frame_time, with_year = TRUE)} (cumulated; o = End of Year)") +
   theme_ebt() +
-  transition_time(Quarter) +
+  transition_time(Semester) +
   ease_aes("quartic-in") +
   shadow_mark() -> p
 
-animate(p, nframes = pull(tally(dat), n) * 10, duration = 10, end_pause = 100, width = 12 * 50, height = 17 * 50,
-        renderer = gifski_renderer("spec/deno_anim.gif"))
+animate(p, nframes = pull(tally(dat), n) * 12, detail = 10, end_pause = 100, width = 12 * 50, height = 17 * 50,
+        renderer = gifski_renderer("deno_anim.gif")) #spec/
 
 rm(dat, p)
