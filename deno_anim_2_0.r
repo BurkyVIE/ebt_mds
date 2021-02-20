67f964a9ba36da274dc24b8943db59a23daf14a5
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2021-02-20 21:18:23 +0100

    Update deno_anim_2_0.r

--- deno_anim_2_0.r
+++ deno_anim_2_0.r
@@ -4,11 +4,11 @@ library(gganimate)
 
 source("ebt_mds_grpd.r")
 
-dat <- ebt_mds_grpd(period = "halfyear")
-dat %>% select(Period, Deno) %>%
-  transmute(Halfyear = Period,
+dat <- ebt_mds_grpd(period = "halfyear", grouping = Halfyear)
+dat %>% select(Halfyear, Deno) %>%
+  transmute(Halfyear,
             map_dfr(Deno, ~set_names(., paste0("EUR_", sprintf("%03d", c(5, 10, 20, 50, 100, 200, 500))))),
-            H2 = case_when(semester(Period) == 2 ~ TRUE,
+            H2 = case_when(semester(Halfyear) == 2 ~ TRUE,
                            TRUE ~ NA)) %>%
   mutate(across(starts_with("EUR_"), cumsum)) %>% 
   pivot_longer(starts_with("EUR_"), names_to = "Denomination", values_to = "Count") %>%
