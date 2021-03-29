5f19c9743cf72bc83602e1f2aa7f947878cc396c
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2021-03-29 22:12:46 +0200

    Update ebt_mds_full.r

--- ebt_mds_full.r
+++ ebt_mds_full.r
@@ -8,16 +8,23 @@ ebt_mds_full <-
   select(-Days, -EntRt, -LocRt, -ERPctl, -LRPctl) %>% # Beide Ableitungen entstehen durch Division durch 1
   mutate(Day = lubridate::wday(x = Date, week_start = 1, label = TRUE) %>% ordered(labels = c("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"))) %>% # Immer englische Abkürzungen für die Wochentage verwenden [wday() nimmt aus locale()]
   relocate(Day, .after = Loc) %>% 
+  add_column(EntryStreak = (.$Count > 0) %>% rle() %>% # ab hier für EntryStreak
+               `[[`(1) %>%
+               rep(1:length(.), .)) %>% 
+  mutate(EntryStreak = paste(ceiling(EntryStreak / 2) %>% sprintf("%02d", .),
+                           case_when(Count == 0 ~ "inactv",
+                                     TRUE ~ "active"),
+                           (Count == 0) %>% rle() %>% `[[`(1) %>% sapply(., seq) %>% unlist() %>% sprintf("%04d", .),
+                           sep ="-")) %>%
   add_column(HitStreak = (.$Hits > 0) %>% rle() %>% # ab hier für HitStreak
                `[[`(1) %>%
                rep(1:length(.), .)) %>% 
-  mutate(HitStreak = paste(ceiling(HitStreak / 2),
-                        case_when(Hits == 0 ~ "inactive",
+  mutate(HitStreak = paste(ceiling(HitStreak / 2) %>% sprintf("%04d", .),
+                        case_when(Hits == 0 ~ "inactv",
                                   TRUE ~ "active"),
-                        (Hits == 0) %>% rle() %>% `[[`(1) %>% sapply(., seq) %>% unlist(),
+                        (Hits == 0) %>% rle() %>% `[[`(1) %>% sapply(., seq) %>% unlist() %>% sprintf("%02d", .),
                         sep ="-")) %>%
-  arrange(desc(Date)) %>% 
-  relocate(AvPctl, .after = Avg)
+  arrange(desc(Date))
 
 
 
