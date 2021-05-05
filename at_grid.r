6b1f3d6b5d4bb12d1cb7e4e1af6ac78f0ddf9626
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2021-05-05 09:25:48 +0200

    Update at_grid.r

--- at_grid.r
+++ at_grid.r
@@ -10,28 +10,36 @@ grid <- crossing(lat = raster$lat %>% head(-1),     # letzter Punkt ergibt sich 
   rowid_to_column(var = "ID") %>% 
   mutate(dot = map2(.x = long, .y = lat,   # corners to all squares (must be closed)
                     .f = ~ matrix(c(..1 + c(0, 1, 1, 0, 0) * 66/157, ..2 + c(0, 0, 1, 1, 0) * 42/150), ncol = 2) %>% list() %>% st_polygon())) %>% 
-  st_as_sf(sf_column_name = "dot", crs = 4326)
+  st_as_sf(sf_column_name = "dot", crs = 4326) %>%
+  st_set_agr("constant")
 rm(raster)
 
 # Country borders
-map_world <- ne_countries(scale = 10, returnclass = "sf")[1]
+map_world <- ne_countries(scale = 10, returnclass = "sf")[1] %>%
+  st_transform(crs = 4326) %>%
+  st_set_agr("constant")
 
 # Map of Austria - or other country/-ies if desired
-map_at <- ne_countries(country = c("Austria"), scale = 10, returnclass = "sf")[1]
+map_at <- ne_countries(country = c("Austria"), scale = 10, returnclass = "sf")[1] %>%
+  st_transform(crs = 4326) %>%
+  st_set_agr("constant")
 
 # Locations
 locs <- pseudo %>%
   select(Loc, Country, Coords) %>% 
   separate(col = Coords, into = c("Long", "Lat"), sep = "~", convert = TRUE) %>% 
-  st_as_sf(coords = c("Long", "Lat"), crs = 4326)
+  st_as_sf(coords = c("Long", "Lat"), crs = 4326) %>%
+  st_set_agr("constant")
 
 # Bounding box for data reduction
+xtra <- 2  # Additional Dots on each side
 #box <- (st_bbox(map_at) + 3 *c(-66/157, -42/150, 66/157, 42/150))[c(1, 1, 3, 3, 1, 2, 4, 4, 2, 2)] %>% # add x times size of one dot to every side
-box <- (c(-12 + 51 * 66/157, 29 + 62 * 42/150, -12 + 70 * 66/157, 29 + 72 * 42/150) + 2.5 *c(-66/157, -42/150, 66/157, 42/150))[c(1, 1, 3, 3, 1, 2, 4, 4, 2, 2)] %>% # add x times size of one dot to every side
+box <- (c(-12 + (53 - xtra) * 66/157, 29 + (64 - xtra) * 42/150, -12 + (68 + xtra) * 66/157, 29 + (70 + xtra) * 42/150) + 2.5 *c(-66/157, -42/150, 66/157, 42/150))[c(1, 1, 3, 3, 1, 2, 4, 4, 2, 2)] %>% # add x times size of one dot to every side
   matrix(ncol = 2) %>%
   list() %>%
   st_polygon() %>%
   st_sfc(crs = 4326)
+rm(xtra)
 
 # Dots mit Location
 been <- st_intersection(grid, locs) %>%
@@ -57,17 +65,17 @@ locs <- st_transform(locs, crs = 3416)
 visited <- st_transform(visited, crs = 3416)
 
 #Farben
-colors <- c("wheat", "grey65", "red3", "grey35", "blue", "blue3")
+colors <- c("wheat", "grey65", "red3", "grey35", "blue3", "mediumblue")
 
 # Plot
 p <- ggplot() +
   geom_sf(data = visited, color = NA, fill = colors[1], alpha = .9) +
-  geom_sf(data = map_world, color = colors[2], size = 3/2, fill = NA) +
+  geom_sf(data = map_world, color = colors[2], size = 2/2, fill = NA) +
   geom_sf(data = map_at, color = colors[3], size = 3/2, fill = NA) +
   geom_sf(data = grid, color = colors[4], fill = NA) +
-  geom_sf(data = locs, mapping = aes(color = Origin), size = 4/4, alpha = 1/2, show.legend = FALSE) +
+  geom_sf(data = locs, mapping = aes(color = Origin), size = 5/4, alpha = 1/2, show.legend = FALSE) +
   scale_color_manual(values = c("AT" = colors[5], "non AT" = colors[6])) +
-#  coord_sf(xlim = st_bbox(map_at)[c(1, 3)], ylim = st_bbox(map_at)[c(2, 4)]) +
+  #  coord_sf(xlim = st_bbox(map_at)[c(1, 3)], ylim = st_bbox(map_at)[c(2, 4)]) +
   labs(title = "Locations / Dots in and around Austria by Burky",
        subtitle = "Projection = WGS 89 / Austria Lambert (EPSG:3416)",
        #caption = "Projection = WGS 84 (EPSG:4326)") +
