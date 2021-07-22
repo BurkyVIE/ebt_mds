98e4d76ca2b7a1702added933404811f9065fbe4
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2021-07-22 11:02:54 +0200

    Update another x bills.r

--- another x bills.r
+++ another x bills.r
@@ -1,19 +1,21 @@
 library(tidyverse)
 
+x <- 5e4  # another x bills
+
 with(ebt_mds_grpd(per = "day", grp_nm = "Date"),
      rep(Date, Count)) %>% 
-  `[`(1:(length(.) %/% 5e4) * 5e4) %>% 
+  `[`(1:(length(.) %/% x) * x) %>% 
   diff() %>% 
   as.numeric() %>% 
   as_tibble_col(., column_name = "Diff") %>% 
   ggplot(mapping = aes(x = Diff)) +
-  geom_histogram(fill = "purple3", color = "purple", binwidth = 5e4/2e3*1.5, size = 1.25) +
+  geom_histogram(fill = "purple3", color = "purple", binwidth = x/2e3*1.5, size = 1.25) +
   geom_boxplot(mapping = aes(y = 1/2), color = "navy", fill = "lightblue", size = 1.25, alpha = .75) +
-  labs(title = paste0("Time to Enter Another ", format(5e4, big.mark = ","), " Bills")) +
+  labs(title = paste0("Time to Enter Another ", format(x, big.mark = ","), " Bills")) +
   xlab("Days") +
   ylab("Count") +
   theme_ebt() -> p
 
 windows(16, 9)
 plot(p)
-rm(p)
+rm(x, p)
