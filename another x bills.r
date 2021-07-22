80d81b955ded000114397d31aa6a420346484a50
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2021-07-22 10:40:19 +0200

    Add files via upload

---
+++ another x bills.r
@@ -0,0 +1,19 @@
+library(tidyverse)
+
+with(ebt_mds_grpd(per = "day", grp_nm = "Date"),
+     rep(Date, Count)) %>% 
+  `[`(1:(length(.) %/% 5e4) * 5e4) %>% 
+  diff() %>% 
+  as.numeric() %>% 
+  as_tibble_col(., column_name = "Diff") %>% 
+  ggplot(mapping = aes(x = Diff)) +
+  geom_histogram(fill = "purple3", color = "purple", binwidth = 5e4/2e3*1.5, size = 1.25) +
+  geom_boxplot(mapping = aes(y = 1), color = "navy", fill = "lightblue", size = 1.25, alpha = .75) +
+  labs(title = paste0("Time to Enter Another ", format(5e4, big.mark = ","), " Bills")) +
+  xlab("Days") +
+  ylab("Count") +
+  theme_ebt() -> p
+
+windows(16, 9)
+plot(p)
+rm(p)
