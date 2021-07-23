8e9e3a072ce2276df54ecbf58ad1cdd76047dc6d
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2021-07-23 12:23:58 +0200

    Update another x bills.r

--- another x bills.r
+++ another x bills.r
@@ -9,8 +9,8 @@ with(ebt_mds_grpd(per = "day", grp_nm = "Date"),
   as.numeric() %>% 
   as_tibble_col(., column_name = "Diff") %>% 
   ggplot(mapping = aes(x = Diff)) +
-  geom_histogram(fill = "purple3", color = "purple", binwidth = x/2e3*1.5, size = 1.25) +
-  geom_boxplot(mapping = aes(y = 1/2), color = "navy", fill = "lightblue", size = 1.25, alpha = .75) +
+  geom_histogram(fill = "purple", color = "purple3", binwidth = x/2e3*1.5, size = 1.25) +
+  geom_boxplot(mapping = aes(y = .25), width = .2, color = "navy", fill = "white", size = 1.25, alpha = .75) +
   labs(title = paste0("EuroBillTracker - Time to Enter Another ", format(x, big.mark = ","), " Bills"),
        subtitle = "by Burky",
        x = "Days",
