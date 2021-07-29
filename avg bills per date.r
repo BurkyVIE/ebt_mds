a878dd76041e76fef0b6b3afc9b41d89ddc5629f
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2021-07-29 13:39:31 +0200

    Add files via upload

---
+++ avg bills per date.r
@@ -0,0 +1,25 @@
+library(tidyverse)
+
+ebt_mds_full %>%
+  mutate(MoDa = str_sub(Date, 6, 10)) %>%
+  group_by(MoDa) %>%
+  summarise(n = n(),
+            Total = sum(Count),
+            Avg = mean(Count),
+            SD = sd(Count)) %>% 
+  mutate(V = SD / Avg,
+         Month = as.numeric(str_sub(MoDa, 1, 2))) %>% 
+  ggplot(mapping = aes(x = Avg)) +
+  geom_density(mapping = aes(y = after_stat(scaled)), fill = "lightblue", color = "navy") +
+  geom_jitter(mapping = aes(y = -.15), height = .06, pch = 21, color = "navy", fill = "lightblue") +
+  geom_boxplot(mapping = aes(y = -.04), width = .06, color = "navy", size = 1.25) +
+  labs(title = "EuroBillTracker - On Average Entered Bills per Date (n = 366)",
+       subtitle = "by Burky",
+       x = "Average") +
+  theme_ebt() +
+  theme(axis.title.y = element_blank(),
+        axis.text.y = element_blank()) -> p
+
+windows(16, 9)
+plot(p)
+rm(p)
