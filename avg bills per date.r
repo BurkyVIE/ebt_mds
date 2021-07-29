e49a4c55ffb26945fde1ffca34a840c9d092c208
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2021-07-29 13:43:43 +0200

    Update avg bills per date.r

--- avg bills per date.r
+++ avg bills per date.r
@@ -11,13 +11,15 @@ ebt_mds_full %>%
          Month = as.numeric(str_sub(MoDa, 1, 2))) %>% 
   ggplot(mapping = aes(x = Avg)) +
   geom_density(mapping = aes(y = after_stat(scaled)), fill = "lightblue", color = "navy") +
-  geom_jitter(mapping = aes(y = -.15), height = .06, pch = 21, color = "navy", fill = "lightblue") +
+  geom_jitter(mapping = aes(y = -.2), height = .11, pch = 21, color = "navy", fill = "lightblue") +
   geom_boxplot(mapping = aes(y = -.04), width = .06, color = "navy", size = 1.25) +
   labs(title = "EuroBillTracker - On Average Entered Bills per Date (n = 366)",
        subtitle = "by Burky",
        x = "Average") +
   theme_ebt() +
-  theme(axis.title.y = element_blank(),
+  theme(panel.grid.minor.y = element_blank(),
+        panel.grid.major.y = element_blank(),
+        axis.title.y = element_blank(),
         axis.text.y = element_blank()) -> p
 
 windows(16, 9)
