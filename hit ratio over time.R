150ad1c691ebb73be91eef908607104a9ad980d8
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2022-10-13 08:24:53 +0200

    Update hit ratio over time.R

--- hit ratio over time.R
+++ hit ratio over time.R
@@ -4,12 +4,13 @@ library(tidyverse)
 # GLOBAL ----
 cuts <- list(
   c(0, 75, 100, 125, 250, 1000, Inf),
-  c("< 75", "< 100", "< 125", "< 250", "< 1000", "≥ 1000"))
+  c("< 75", "< 100", "< 125", "< 250", "< 1000", "≥ 1000"),
+  right_closed = FALSE)
 
 # DATA ----
 dat <- ebt_mds_grpd(period = "day", grp_nm = "Date") |>
   transmute(Date, cCount = cumsum(Count), cHits = cumsum(Hits)) |>
-  mutate(cHitRt = cCount / cHits, Set = cut(cHitRt, right = FALSE, breaks = cuts[[1]], labels = cuts[[2]]))
+  mutate(cHitRt = cCount / cHits, Set = cut(cHitRt, right = cuts[[3]], breaks = cuts[[1]], labels = cuts[[2]]))
   
 # PLOT ----
 p <- ggplot(data = dat) +
