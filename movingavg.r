e17bafd91b931dc9ace81123779185d94271c282
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2022-11-02 13:06:10 +0100

    Update movingavg.r

--- movingavg.r
+++ movingavg.r
@@ -12,9 +12,10 @@ ebt_mds_grpd(per = "day", grp_nm = "Date") |>
   tail(param[1]) %>%
   ggplot() +
   aes(x = Date) +
-  geom_col(mapping = aes(y = !!vari), fill = "lightblue", color = "navy") +
+  geom_col(mapping = aes(y = !!vari), fill = "#1074B3", color = "#0E4466") +
   geom_line(mapping = aes(y = Avg), color = "firebrick", lwd = 1.5) +
-  scale_x_date(date_labels = "%m/%Y") +
+  scale_x_date(date_labels = "%m/%Y", expand = expansion(add = c(3, 1))) +
+  scale_y_continuous(breaks = function(x)seq(from = 0, to = x[2], by = floor(x[2]/4))) +
   labs(title = paste0("EuroBillTracker - Activity Over the Past ", param[1], " Days, including a ", param[2], "-day Moving Average"),
        subtitle = "by Burky",
        caption = paste0("as: ",max(ebt_mds$Date) ," (https://www.eurobilltracker.com)")) +
