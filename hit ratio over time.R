b274f10d75ca09d884c19f65175d497ede405ec8
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2022-11-30 15:19:35 +0100

    Update hit ratio over time.R

--- hit ratio over time.R
+++ hit ratio over time.R
@@ -20,15 +20,28 @@ dat <- ebt_mds_grpd(period = "day", grp_nm = "Date") |>
                             TRUE ~ "equal"))
 dat <- mutate(dat, Set = cut(cHitRt, right = cuts[[3]], breaks = cuts[[1]], labels = cuts[[2]]))
 
+# Loakale Extrema
+n_extr <- 7
+extreme <- mutate(dat,
+                  Extreme = case_when(Change == "lower" ~ "-",
+                                      Change == "higher" ~ "+",
+                                      TRUE ~ "0"),
+                  # Extreme = paste0(lag(Extreme, 5), lag(Extreme, 4), lag(Extreme, 3), lag(Extreme, 2), lag(Extreme, 1),
+                  Extreme = paste0(map_dfc(set_names(n_extr:1, letters[1:n_extr]), .f = ~ lag(Extreme, .), ) |> (\(x) apply(x, 1, paste, collapse = ""))(),
+                                   Extreme,
+                                   map_dfc(set_names(1:n_extr, letters[1:n_extr]), .f = ~ lead(Extreme, .)) |> (\(x) apply(x, 1, paste, collapse = ""))())) |> 
+  filter(Extreme %in% c(paste(c(rep("-", n_extr + 1), rep("+", n_extr)), collapse = ""), paste(c(rep("+", n_extr + 1), rep("-", n_extr)), collapse = "")))
+
 spec0 <- filter(dat, is.finite(cHitRt))
 
 spec <- bind_rows(
   filter(spec0, row_number() == 1),
   filter(spec0, cHitRt == min(cHitRt)),
   filter(spec0, cHitRt == max(cHitRt)),
-  filter(spec0, row_number() == n())) |>
+  filter(spec0, row_number() == n()),
+  extreme) |>
   mutate(Label = format(cHitRt, trim = TRUE, digits = 2, nsmall = 2, big.mark = ","),
-         Label = paste(c("first", "lowest", "highest", "latest"), Label, sep = ": "))
+         Label = paste(c("first", "lowest", "highest", "latest", rep("local extreme", tally(extreme))), Label, sep = ": "))
   
 # PLOT ----
 p <- ggplot(data = dat) +
@@ -38,8 +51,8 @@ p <- ggplot(data = dat) +
   ggrepel::geom_label_repel(data = spec, mapping = aes(label = Label),
                             box.padding = 1.5, nudge_y = -0.25, direction = "x",
                             size = 2.75, fill = rgb(221, 226, 233, maxColorValue = 255), alpha = 3/4, show.legend = FALSE) +
-  scale_x_date(name = "Time", date_minor_breaks = "year", expand = c(1/100, 0)) +
-  scale_y_log10(name = "Cumulative Hit Ratio [log10]", labels = function(x)format(x, big.mark = ",")) +
+  scale_x_date(name = "Time", date_minor_breaks = "year", expand = expansion(add = c(30, 90))) +
+  scale_y_log10(name = "Cumulative Hit Ratio [log10]", labels = function(x)format(x, big.mark = ","), expand = expansion(mult = .01)) +
   scale_fill_brewer(name = "Hit Ratio", palette = "RdYlGn", direction = -1, na.value = "grey50") +
   labs(title = "EuroBillTracker - Hit Ratio over Time",
        subtitle = "by Burky",
@@ -51,4 +64,4 @@ windows(16, 9, restoreConsole = TRUE)
 plot(p)
 
 # CLEAN UP ----
-rm(cuts, dat, spec0, spec, p)
+rm(cuts, dat, n_extr, extreme, spec0, spec, p)
