b26d95cb41efee84062e2c1aa8b8ee93925d81ec
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2022-12-05 07:47:07 +0100

    Update yearly entry rates_2.r

--- yearly entry rates_2.r
+++ yearly entry rates_2.r
@@ -9,7 +9,7 @@ param <- list(list(period = "day", xmax = 365, y_fn = function(x) year(x), t_fn 
               list(period = "week", xmax = 52, y_fn = function(x) year(x + days(3)), t_fn = function(x) isoweek(x), by = 8), # 3 wg Mittwoch der Woche; dieser entscheidet ob Woche 1 im Jahr+1 oder Woche 53
               list(period = "month", xmax = 12, y_fn = function(x) year(x), t_fn = function(x) month(x), by = 2)
               )[[1]] # <- HIER Schalter!
-# period    ... Für die ebt_mds_grp- Abfrage und für die Beschriftung x-Achse
+# period    ... Für die ebt_mds_grpd-Abfrage und für die Beschriftung x-Achse
 # xmax      ... x-Achsen-Korrektur für Labels Hilfslinien
 # y_fn      ... Funktion zur Berechnung des Jahres (Gruppierung)
 # t_fn      ... Berechnung der x-Abschnitte
