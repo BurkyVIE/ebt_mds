836b4afb3141044866d539b7d8b00b9e28ba1e45
Thomas Burkhardt <39870358+BurkyVIE@users.noreply.github.com>
2024-06-13 09:33:40 +0200

    Update another x bills.r

--- another x bills.r
+++ another x bills.r
@@ -1,24 +1,24 @@
 library(tidyverse)
 
-divisor <- c(Cnt = 1, Val = 25, Hts = 1/100) * 25e3
+divisor <- c(Count = 1, Value = 25, Hits = 1/100) * 25e3
 
 he <- ebt_mds_grpd(per = "day", grp_nm = "Date") |> 
-  select(Date, Cnt = Count, Val = Value, Hts = Hits) |> 
-  mutate(across(.cols = Cnt:Hts, .fns = ~ cumsum(.), .names = "C_{.col}"),
+  select(Date, Count, Value, Hits) |> 
+  mutate(across(.cols = Count:Hits, .fns = ~ cumsum(.), .names = "C_{.col}"),
          across(.cols = starts_with("C_"), .fns = ~ c(0, diff(
-           . %/% .GlobalEnv$divisor[substr(deparse(substitute(.)), 3, 5)]) # deparse(substitute) gibt variablen namen aus across als string
-           ) == 1, .names = "{.col}_mod")) |>
-  filter(if_any(ends_with("_mod"))) |> 
-  add_row(Date = lubridate::ymd("2004-8-3"), C_Cnt_mod = TRUE, C_Val_mod = TRUE, C_Hts_mod = TRUE, .before = 1)
+           . %/% .GlobalEnv$divisor[str_replace(deparse(substitute(.)), ".*_(.*)","\\1")]) # deparse(substitute) gibt variablen namen aus across als string
+           ) == 1, .names = "{.col}_ns")) |>
+  filter(if_any(ends_with("ns"))) |> 
+  add_row(Date = lubridate::ymd("2004-8-3"), C_Count_ns = TRUE, C_Value_ns = TRUE, C_Hits_ns = TRUE, .before = 1)
 
-select(he, Date, ends_with("_mod")) |>
+select(he, Date, ends_with("ns")) |>
   pivot_longer(-Date, names_to = "Cat", values_to = "filter") |>
   filter(filter) |>
-  mutate(DDiff = c(NA, diff(Date)), .by = Cat,
-         Cat = factor(substr(Cat, 3, 5), levels = c("Cnt", "Val", "Hts"),
-                      labels = c(paste0(divisor["Cnt"] / 1e3, "k Bills"),
-                                 paste0(divisor["Val"] / 1e3, "k Euro"),
-                                 paste0(divisor["Hts"], " Hits")))) |> 
+  mutate(DDiff = c(NA, diff(Date)), .by = Cat, .keep = "used") |> 
+  mutate(Cat = factor(str_replace(Cat, ".*_(.*)_.*", "\\1"), levels = names(divisor),
+                      labels = c(paste0(divisor["Count"] / 1e3, "k Bills"),
+                                 paste0(divisor["Value"] / 1e3, "k Euro"),
+                                 paste0(divisor["Hits"], " Hits")))) |> 
   filter(!is.na(DDiff)) |> 
   ggplot(mapping = aes(x = DDiff, fill = Cat)) +
   geom_histogram(binwidth = 30, color = "white", show.legend = FALSE) +
